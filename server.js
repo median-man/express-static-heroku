5ca2a4cb8165c2cacb7242fa96e3bb151a9b9d17
John Desrosiers <john.desro@gmail.com>
2021-07-15 14:47:44 -0700

    Add static middleware to server

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 app.listen(port, () => {
   console.log(`Example app listening at port ${port}`);
